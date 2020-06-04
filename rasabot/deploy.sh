2408f2af4e6ef13cc7a15409ef59de5067d9d640
Michael Fekadu <mike.fekadu@gmail.com>
2020-06-04 16:43:44 -0700

    docker rasa runs!

--- rasabot/deploy.sh
+++ rasabot/deploy.sh
@@ -1,1 +1,11 @@
-docker run 582rasa run 
+# make a network for main rasa and rasa action server to talk
+# https://rasa.com/docs/rasa/user-guide/docker/building-in-docker/
+docker network create rasa-network 
+
+# run rasa in its own container in the background
+docker run --detach --publish 5005:5005 --net rasa-network 582rasa run 
+
+# run rasa action server in its own container in the background
+docker run --detach --publish 5055:5055 --net rasa-network 582rasa \
+    run -vv actions --debug
+
---
+++ rasabot/run_bash.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+## --entrypoint
+#  --entrypoint string              Overwrite the default ENTRYPOINT of the image
+docker run -it --entrypoint /bin/bash 582rasa
